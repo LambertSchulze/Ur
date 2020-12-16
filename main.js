1b9a00640b4b950905e7ba9e5b36a71a15792188
Lambert Schulze <schulze.lambert@googlemail.com>
2020-12-17 00:29:46 +0100

    added console input test

--- main.js
+++ main.js
@@ -30,3 +30,25 @@ S  ║ ¤                 « 7     0 ¦ ¤       ║
 };
 
 printBoard()
+
+const diceThrow = () => {
+	return Math.floor (Math.random() * 2) +
+	       Math.floor (Math.random() * 2) +
+	       Math.floor (Math.random() * 2) +
+	       Math.floor (Math.random() * 2);
+}
+
+console.log ("Roll Test: " + diceThrow() );
+
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Which piece do should move? ', (answer) => {
+  console.log(`You chose: ${answer}`);
+  rl.close();
+});
