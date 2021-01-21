34f69cd8bb1d94ca76ee3fe6daa152cd01cc5f1f
Lambert Schulze <schulze.lambert@googlemail.com>
2021-01-21 02:24:39 +0100

    newgame as actioncreator in redux reducer

--- client/src/components/MainMenu.js
+++ client/src/components/MainMenu.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { makeNewGame } from './../reducers/gameReducer'
 
 const MainMenu = () => {
+  const dispatch = useDispatch()
+
+  const startNewGame = (event) => {
+    event.preventDefault()
+    dispatch(makeNewGame())
+  }
+
   return (
     <nav id='MainMenu'>
       <ul>
-        <li><button id='NewGame'>New Game</button></li>
+        <li><button id='NewGame' onClick={startNewGame}>New Game</button></li>
         <li><button id='GameRules'>How to play</button></li>
       </ul>
     </nav>
